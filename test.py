29baa24da04aeb6b14bf6ab1f9b0b0503b704e9d
Elwin Cabrera <elwincab0@gmail.com>
2020-06-05 14:51:04 -0400

    doing some prelimenary testing

--- test.py
+++ test.py
@@ -1,73 +1,71 @@
+"""
+PI power
+5V on pin
+GND on pin
+
+The GPIO mode is set to BCM
+
+
+H-Bridge Motor Driver Pin Configuration
+in1    -> BCM --   (board pin 29 or GPIO )
+in2    -> BCM --   (board pin 31 or GPIO )
+enable -> BCM --   (board pin 33 or GPIO )
+
+PCA9685 (16-Channel Servo Driver) Pin Configuration
+SCL
+SDA
+VCC
+GND
+
+HC-SR04 (Sonar Distance Sensor)
+Trig
+Echo
+VCC
+GND
+
+
+"""
+
 from adafruit_servokit import ServoKit
-#from gpiozero import Motor
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Motor, CompositeDevice
+from time import sleep
+from enum import Enum
 
-#GPIO.setmode(GPIO.BOARD)
-input1 = 29
-input2 = 31
-enable = 33
-
-#GPIO.setup(input1, GPIO.OUT)
-#GPIO.setup(input2, GPIO.OUT)
-#GPIO.setup(enable, GPIO.OUT)
-
-#en = GPIO.PWM(enable, 5000)
-MIN_START_DUTY = 35
-#motor = Motor(forward=11, backward=13)
 kit = ServoKit(channels=16)
 
-WHEELS = 0
-PAN = 1
-TILT = 2
 
-TOP_RIGHT_HYDR = 4
-TOP_LEFT_HYDR = 5
-BOTTOM_RIGHT_HYDR = 6
-BOTTOM_LEFT_HYDR = 7
+class Ch(Enum):
+    STEERING = 0
+    CAM_PAN = 1
+    CAM_TILT = 2
 
-try:
-#	en.start(100)
-#	time.sleep(0.5)
+    TRIGHT_HYDR = 4
+    TLEFT_HYDR = 5
+    BRIGHT_HYDR = 6
+    BLEFT_HYDR = 7
 
-	#kit.servo[PAN].angle = 0
-	#kit.servo[TILT].angle = 0
-	#kit.servo[TOP_RIGHT_HYDR].angle = 0
-	#kit.servo[TOP_LEFT_HYDR].angle = 0
-	#kit.servo[BOTTOM_RIGHT_HYDR].angle = 0
-	#kit.servo[BOTTOM_LEFT_HYDR].angle = 0
 
-	while True:
-		servoNum = PAN
-		for i in range(0,180,5):
-			#print("Servo: ",servoNum,"Angle:",i)
-	       		kit.servo[servoNum].angle=i
-	        	time.sleep(0.05)
+class HBridgeMotorDriver:
+    def __init__(self):
+        self.in1 = 29
+        self.in2 = 31
+        self.enable = 33
 
-		#GPIO.output(enable, GPIO.HIGH)
 
-		#GPIO.output(input1, GPIO.HIGH)
-		#GPIO.output(input2, GPIO.LOW)
-		#time.sleep(3)
-#		GPIO.output(input1, GPIO.LOW)
-#		GPIO.output(input2, GPIO.HIGH)
-		#time.sleep(3)
+class Servos:
+    def __init__(self):
+        self.scl = 0
+        self.sda = 0
+        self.vcc = 0
+        self.gnd = 0
 
-		#for duty in range(MIN_START_DUTY,101, 5):
-		#	print("Changing duty Cycle to:",duty)
-		#	en.ChangeDutyCycle(duty)
-		#	time.sleep(1)
 
-#		for duty in range (100, MIN_START_DUTY-1, -5):
-#			print("Changing duty Cycle to:",duty)
-#			en.ChangeDutyCycle(duty)
-#			time.sleep(2)
-#		print("stopping for 3 seconds")
-		#en.stop()
-		#en.ChangeDutyCycle(MIN_START_DUTY)
-#		time.sleep(1)
-except KeyboardInterrupt:
-	print("Program Stopped via keyboard interrupt")
-	en.stop()
-	GPIO.output(enable, GPIO.LOW)
-	GPIO.cleanup()
+class DistanceSensor:
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        print("")
+    except KeyboardInterrupt:
+        print("Program Stopped via keyboard interrupt")
